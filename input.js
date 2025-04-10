d91a6642b4c7cf5880f810f78d41e26fcde2dfc2
Brendan Brown <brendan94brown@gmail.com>
2025-04-09 22:23:21 -0700

    Set up 'W, A, S, D' movement keys, and imported functionality to the play.js file

--- input.js
+++ input.js
@@ -1,10 +1,29 @@
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   };
+  if (key === "w") {
+    console.log("Move: up");
+    connection.write("Move: up");
+  };
+  if (key === "a") {
+    console.log("Move: left");
+    connection.write("Move: left");
+  };
+  if (key === "s") {
+    console.log("Move: down");
+    connection.write("Move: down");
+  };
+  if (key === "d") {
+    console.log("Move: right");
+    connection.write("Move: right");
+  };
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin; 
   stdin.setRawMode(true);      
   stdin.setEncoding("utf8");   
--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 const connect = require("./client");
 const setupInput = require("./input.js")
 
-
 console.log("Connecting ...");
-connect();
+
+const conn = connect();
+setupInput(conn);
