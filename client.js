d13a84704425d20b47b0869c9a0fbae3859fd53d
Brendan Brown <brendan94brown@gmail.com>
2025-04-09 20:59:00 -0700

    Added connection message, as well as conn.write naming callback

--- client.js
+++ client.js
@@ -9,6 +9,12 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: BPB");
+  });
+  
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
