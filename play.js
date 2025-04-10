9792442912ae27cf85afe2c45d9343dd4ccb9eee
Brendan Brown <brendan94brown@gmail.com>
2025-04-09 22:06:56 -0700

    Created input.js file, migrated setupInput and handleUserInput from play.js into input.js

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
 const connect = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
-
-const setupInput = function () {
-  const stdin = process.stdin; 
-  stdin.setRawMode(true);      
-  stdin.setEncoding("utf8");   
-  stdin.resume();  
-  stdin.on("data", handleUserInput);    
-  return stdin;                
-};
+const setupInput = require("./input.js")
 
 
 console.log("Connecting ...");
